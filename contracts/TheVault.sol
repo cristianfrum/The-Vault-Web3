e52f9e463645b6b7fbfe8ae415bb9dd3575c8fdd
cristianfrum <cristian.frum29@gmail.com>
2023-02-07 16:53:32 +0200

    Improved the smart contract
    Built:
    
    - Getter for wallet member counter
    - Getter for wallet members
    - Modifier to avoid wallet duplicates
    - Modifier to avoid an address join multiple wallets
    
    Also, changed members map behavior to become iterable. It should turn into an array in the next update.

--- contracts/TheVault.sol
+++ contracts/TheVault.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.9;
 
 contract TheVault {
     mapping(string => Wallet) public wallet;
+    mapping(address => uint256) public memberWalletId;
     uint8 public walletCounter;
 
     struct Transaction {
@@ -17,7 +18,7 @@ contract TheVault {
         string lastName;
         address currentAddress;
         uint256 walletId;
-        uint8 balance;
+        uint256 balance;
         uint8 withdrawalLimit;
     }
 
@@ -26,8 +27,9 @@ contract TheVault {
         uint256 creationDate;
         string name;
         uint256 balance;
+        uint8 memberCounter;
         Transaction[] transactionHistory;
-        mapping(address => Member) member;
+        mapping(uint8 => Member) members;
     }
 
     function getWalletId(string memory walletName)
@@ -46,32 +48,85 @@ contract TheVault {
         return wallet[walletName].balance;
     }
 
+    function getWalletMemberCounter(string memory walletName)
+        public
+        view
+        returns (uint8)
+    {
+        return wallet[walletName].memberCounter;
+    }
+
+    function getWalletMembers(string memory walletName)
+        public
+        view
+        returns (string[] memory)
+    {
+        address[] memory membersAddresses = new address[](
+            wallet[walletName].memberCounter
+        );
+        string[] memory membersFirstNames = new string[](
+            wallet[walletName].memberCounter
+        );
+        string[] memory membersLastNames = new string[](
+            wallet[walletName].memberCounter
+        );
+        for (uint8 i = 0; i < wallet[walletName].memberCounter; i++) {
+            membersFirstNames[i] = wallet[walletName].members[i].firstName;
+            membersLastNames[i] = wallet[walletName].members[i].lastName;
+            membersAddresses[i] = wallet[walletName].members[i].currentAddress;
+        }
+        return membersFirstNames;
+    }
+
+    modifier checkMemberRedundancy(
+        address[] memory membersAddresses,
+        uint256 memberCounter
+    ) {
+        require(memberCounter > 0, "A wallet needs at least one user");
+        for (uint256 i = 0; i < memberCounter; i++) {
+            require(
+                memberWalletId[membersAddresses[i]] == 0,
+                "One or more users have already joined another wallet"
+            );
+        }
+        _;
+    }
+
+    modifier checkWalletRedundancy(string memory walletName) {
+        require(wallet[walletName].id == 0, "A wallet already has this name");
+        _;
+    }
+
     function initializeWallet(
         string memory walletName,
         address[] memory membersAddresses,
         string[] memory membersFirstNames,
         string[] memory membersLastNames
-    ) public payable {
+    )
+        public
+        payable
+        checkMemberRedundancy(membersAddresses, membersFirstNames.length)
+        checkWalletRedundancy(walletName)
+    {
+        // wallet id starts with 100 instead of 0 because users with memberWalletId set to 0 do not exist yet
         Wallet storage newWallet = wallet[walletName];
-        newWallet.id = walletCounter;
+        newWallet.id = walletCounter + 100;
         newWallet.creationDate = block.timestamp;
         newWallet.name = walletName;
         newWallet.balance = msg.value;
 
-        for (uint256 i = 0; i < membersAddresses.length; i++) {
-            Member storage newMember = newWallet.member[membersAddresses[i]];
+        for (uint8 i = 0; i < membersAddresses.length; i++) {
+            Member storage newMember = newWallet.members[i];
             newMember.firstName = membersFirstNames[i];
             newMember.lastName = membersLastNames[i];
             newMember.currentAddress = membersAddresses[i];
-            newMember.walletId = walletCounter;
+            newMember.walletId = walletCounter + 100;
             newMember.balance = 0;
             newMember.withdrawalLimit = 5;
+            newWallet.memberCounter++;
+
+            memberWalletId[newMember.currentAddress] = newMember.walletId;
         }
         walletCounter++;
-
-        /*  for (uint i = 0; i < initialMembers.length; i++) {
-            newWallet.members[initialMembers[i].currentAddress] = initialMembers[i];
-        }
-      */
     }
 }
